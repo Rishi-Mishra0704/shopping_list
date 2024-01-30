dc7d3ec32384106d54b7871510006f228e7084d0
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-30 23:01:42 +0530

    add error handling

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -16,6 +16,7 @@ class GroceryList extends StatefulWidget {
 class _GroceryListState extends State<GroceryList> {
   List<GroceryItem> _groceryItems = [];
   var _isLoading = true;
+  String? _error;
 
   @override
   void initState() {
@@ -28,6 +29,11 @@ class _GroceryListState extends State<GroceryList> {
         'shop-app-1348d-default-rtdb.asia-southeast1.firebasedatabase.app',
         'shopping-list.json');
     final response = await http.get(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _error = 'Failed to fetch data. Try again later';
+      });
+    }
     final Map<String, dynamic> listData = json.decode(response.body);
     final List<GroceryItem> loadedItems = [];
     for (final item in listData.entries) {
@@ -106,6 +112,12 @@ class _GroceryListState extends State<GroceryList> {
         ),
       );
     }
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
