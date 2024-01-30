3ae813002358c972514064b67938284d7ec5b5c8
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2024-01-30 16:55:40 +0530

    add fallback text

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -32,12 +32,11 @@ class _GroceryListState extends State<GroceryList> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your Groceries'),
-        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
-      ),
-      body: ListView.builder(
+    Widget content = const Center(
+      child: Text('No Items Added Yet'),
+    );
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
         itemCount: _groceryItems.length,
         itemBuilder: (ctx, index) => ListTile(
           title: Text(_groceryItems[index].name),
@@ -50,7 +49,14 @@ class _GroceryListState extends State<GroceryList> {
             _groceryItems[index].quantity.toString(),
           ),
         ),
+      );
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: const Icon(Icons.add))],
       ),
+      body: content,
     );
   }
 }
